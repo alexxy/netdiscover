d3498ebf402d75eb8cc54903a54d7d1469e6880c
Jaime Peñalba <jpenalbae@gmail.com>
2011-06-23 08:57:55 +0000

    Larry Reznick <lreznick@rezfam.com>: oui script fixes for redhat and code reformatting to make it more readable including manpage

--- update-oui-database.sh
+++ update-oui-database.sh
@@ -31,44 +31,105 @@ AWK="gawk"
 #AWK="awk"
 
 [ -d "$DSTD" ] || { echo "$JA: Destdir \"$DSTD\" not exist!"; exit 1; }
+#if ! [ -f "$TMPF" -a -s "$TMPF" ]; then
+#   echo "Trying download \"$ORIGF\" with lynx..."
+#   if ! lynx -source $URL >"$TMPF"; then
+#      echo "Trying download \"$ORIGF\" with elinks..."
+#      if ! elinks -source $URL >"$TMPF"; then
+#         echo "Trying download \"$ORIGF\" with wget..."
+#         if ! wget --quiet --output-document="$TMPF" $URL; then
+#            echo "$JA: Cann't obtain \"$URL\"!"
+#            exit 1
+#         fi
+#      fi
+#   fi
+#else
+#   echo "\"$TMPF\" already exist, skipping download..."
+#fi
 if ! [ -f "$TMPF" -a -s "$TMPF" ]; then
-   echo "Trying download \"$ORIGF\" with lynx..."
-   if ! lynx -source $URL >"$TMPF"; then
-      echo "Trying download \"$ORIGF\" with elinks..."
-      if ! elinks -source $URL >"$TMPF"; then
-         echo "Trying download \"$ORIGF\" with wget..."
-         if ! wget --quiet --output-document="$TMPF" $URL; then
-            echo "$JA: Cann't obtain \"$URL\"!"
-            exit 1
-         fi
-      fi
-   fi
+  echo -n "Trying download \"$ORIGF\" with lynx..."
+  if [[ -x /usr/bin/lynx ]]; then
+    lynx -source $URL >"$TMPF"
+  else
+     echo -n " with elinks..."
+     if [[ -x /usr/bin/elinks ]]; then
+       elinks -source $URL >"$TMPF"
+     else
+        echo " with wget..."
+        if [[ -x /usr/bin/wget ]]; then
+          wget --quiet --output-document="$TMPF" $URL
+        else
+           echo "$JA: Can't obtain \"$URL\"!"
+           exit 1
+        fi
+     fi
+  fi
 else
-   echo "\"$TMPF\" already exist, skipping download..."
+   echo -n "\"$TMPF\" already exist, skipping download..."
 fi
+echo ""
+
 echo "Process oui.txt (\"$TMPF\")..."
-# if RS is null string, then records are separated by blank lines... but this isn't true in oui.txt :
-cat "$TMPF"|tee aaa|LANG=C $AWK --re-interval --assign URL="$URL" 'BEGIN{RS="\n([[:blank:]]*\n)+";FS="\n";MI="";NN=0;
- printf "/*\n * Organizationally Unique Identifier list at date %s\n * Automatically generated from %s\n * For Netdiscover by Jaime Penalba\n *\n */\n\nstruct oui {\n   char *prefix;   /* 24 bit global prefix */\n   char *vendor;   /* Vendor id string     */\n};\n\nstruct oui oui_table[] = {\n",strftime("%d-%b-%Y"),URL
+
+# if RS is null string, then records are separated by blank lines...
+# but this isn't true in oui.txt
+
+LANG=C $AWK --re-interval --assign URL="$URL" '
+BEGIN {
+	RS = "\n([[:blank:]]*\n)+";
+	FS = "\n";
+	MI = "";
+	NN = 0;
+	printf( \
+	  "/*\n" \
+	  " * Organizationally Unique Identifier list at date %s\n" \
+	  " * Automatically generated from %s\n" \
+	  " * For Netdiscover by Jaime Penalba\n" \
+	  " *\n" \
+	  " */\n" \
+	  "\n" \
+	  "struct oui {\n" \
+	  "   char *prefix;   /* 24 bit global prefix */\n" \
+	  "   char *vendor;   /* Vendor id string     */\n" \
+	  "};\n" \
+	  "\n" \
+	  "struct oui oui_table[] = {\n", strftime("%d-%b-%Y"), URL);
 }
-(/[[:xdigit:]]{6}/){
-    N1=split($1,A1,/\t+/)
-    N2=split($2,A2,/\t+/)
-    N3=split(A2[1],PN,/ +/)
-#    printf "%i,%i,%i>%s<>%s<>%s< $1=%s<, $2=%s<, $3=%s<.\n",N1,N2,N3,PN[1],A1[2],A2[2],$1,$2,$3
-#    V1=gensub(/^[[:punct:]]+/,"",1,A1[2])
-#    V2=gensub(/^[[:punct:]]+/,"",1,A2[2])
-    V1=gensub(/^[[:blank:]]+/,"",1,A1[2])
-    V2=gensub(/^[[:blank:]]+/,"",1,A2[2])
-    V0=V2; if (V0 ~ /^[[:blank:]]*$/){ V0=V1 }
-    V=gensub(/\"/,"\\\\\"","g",V0)
-    if (MI !="")printf "   { \"%s\", \"%s\" },\n",MI,MV
-    MI=PN[1]; MV=V; NN++
+
+(/[[:xdigit:]]{6}/) {
+	N1 = split($1,A1,/\t+/);
+	N2 = split($2,A2,/\t+/);
+	N3 = split(A2[1],PN,/ +/);
+#	printf("%i,%i,%i>%s<>%s<>%s< $1=%s<, $2=%s<, $3=%s<.\n",N1,N2,N3,PN[1],A1[2],A2[2],$1,$2,$3);
+#	V1 = gensub(/^[[:punct:]]+/,"",1,A1[2]);
+#	V2 = gensub(/^[[:punct:]]+/,"",1,A2[2]);
+	V1 = gensub(/^[[:blank:]]+/,"",1,A1[2]);
+	V2 = gensub(/^[[:blank:]]+/,"",1,A2[2]);
+	V0 = V2;
+	if (V0 ~ /^[[:blank:]]*$/) {
+		V0 = V1;
+	}
+	V = gensub(/\"/,"\\\\\"","g",V0);
+	if (MI != "")
+		printf("   { \"%s\", \"%s\" },\n", MI, MV);
+	MI = PN[1];
+	MV = V;
+	NN++;
 }
-END{printf "   { \"%s\", \"%s\" },\n   { NULL, NULL }\n};\n\n// Total %i items.\n",MI,MV,NN}' >"$DSTD/$DSTF"
+
+END {
+	printf( \
+	  "   { \"%s\", \"%s\" },\n" \
+	  "   { NULL, NULL }\n" \
+	  "};\n" \
+	  "\n" \
+	  "// Total %i items.\n", MI, MV, NN);
+}' <"$TMPF" >"$DSTD/$DSTF"
 
 if [ $? -ne 0 ]; then
-  echo "$JA: $TMPF parsing error !"; exit 1
+  echo "$JA: $TMPF parsing error !"
+  exit 1
 else
   echo "All OK"
+  ls -oh oui.txt-* src/oui.h
 fi
